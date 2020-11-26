3cf1825a59c0338216c161c1d4db9f4b398e967d
Clayton Vogele <vogel507@umn.edu>
2020-11-26 01:45:43 -0600

    initial files for lab 7

---
+++ rot5_13.cpp
@@ -0,0 +1,8 @@
+#include "rot5_13.h"
+
+void rot5_rot13(char& ch) {
+
+   // Stub — Do nothing...
+   ch = ch; // This “do nothing” statement is here to stop g++ from
+            // issuing 'warning: unused parameter ‘ch’'.
+}
