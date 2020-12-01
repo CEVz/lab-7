62461594bc3c41500b0b4614c601cbae2cc9d7e5
Clayton Vogele <vogel507@umn.edu>
2020-11-30 19:58:09 -0600

    completed rot 5 and rot 13 encryption

--- rot5_13.cpp
+++ rot5_13.cpp
@@ -10,36 +10,37 @@ void rot5_rot13(char& ch) {
 }
 
 void rot5_13(std::string encrypt, std::string decipher, std::string zeroToNine){
-
+  
   std::locale loc;
   int key = 13;
-
+  std::string rot5 = "5678901234";
+    
   for (std::string::size_type i = 0; i < encrypt.length(); i++){
     encrypt[i] = std::toupper(encrypt[i], loc);
   }
-
-  std::cout << encrypt << std::endl;
-
-  std::regex intReg("[\\d\\s]+");
-
+    
   for (int i = 0; i < encrypt.length(); i++){
     int temp = encrypt[i] + key;
-    if (encrypt[i] == 32){
+    if (isdigit(encrypt[i]) == true){
+      for (int j = 0; j < zeroToNine.length(); j++){
+	if (encrypt[i] == rot5[j]){
+	  decipher += zeroToNine[j];
+	}
+      }
+    }
+
+    else if (encrypt[i] == 32){
       decipher += " ";
     }
     else if (temp > 90){
       temp -= 26;
-      decipher += (char)temp;
+      decipher += char(temp);
     }
-    else {
-      for (int j = 0; j < zeroToNine.length(); i++){
-	if (encrypt[i] == zeroToNine[j]){
-	  decipher += (int)temp;
-	}
-      }
+    else{
+      decipher += char(temp);
     }
   }
-
-  std::cout << decipher << std::endl;
   
+  std::cout << decipher << std::endl;
+
 }
